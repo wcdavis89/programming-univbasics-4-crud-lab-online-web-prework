f15dd9f2f1dba764da6d1e8b4f3a8d426198b4dc
Whittney Davis <wcdavis89@yahoo.com>
2019-10-30 20:52:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,11 +33,13 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
- best_pets[0]
+  array=["wow", "I", "am", "really", "learning", "arrays!"]
+ array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  best_pets[3]
+ array=["wow", "I", "am", "really", "learning", "arrays!"]
+  array[4]
 end
 
 def update_element_from_index(array, index_number, element)
