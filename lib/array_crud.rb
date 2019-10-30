a3899fd7f98e29af9e688158f067ffda1c2d911d
Whittney Davis <wcdavis89@yahoo.com>
2019-10-30 20:53:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,7 +39,7 @@ end
 
 def retrieve_last_element_from_array(array)
  array=["wow", "I", "am", "really", "learning", "arrays!"]
-  array[4]
+  array[5]
 end
 
 def update_element_from_index(array, index_number, element)
