614ca246fd1fc7eb413480fa6268736001fc4ad0
Whittney Davis <wcdavis89@yahoo.com>
2019-10-30 20:32:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,27 +11,27 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  best_pets.unshift
+  best_pets.unshift("Sir Wubbles McBasscannon V")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  best_pets.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+ best_pets.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  best_pets[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+ best_pets[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  best_pets[3]
 end
 
 def update_element_from_index(array, index_number, element)
