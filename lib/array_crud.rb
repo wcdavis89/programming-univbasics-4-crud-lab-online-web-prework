977efbded3116bdcbfc97096711f3e8d73bdaba1
Whittney Davis <wcdavis89@yahoo.com>
2019-10-30 20:54:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,5 +43,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-best_pets[4]="totally"
+array=["wow", "I", "am", "really", "learning", "arrays!"]
+array[3]="totally"
 end
