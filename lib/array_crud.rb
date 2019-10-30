68675ce764d087a9f454d05e2053b95cae80e9ac
Whittney Davis <wcdavis89@yahoo.com>
2019-10-30 20:36:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  best_pets["Luci", "Rico", "Zipper", "Fiesta"]
+  best_pets=["Luci", "Rico", "Zipper", "Fiesta"]
 end
 
 def add_element_to_end_of_array(array, element)
