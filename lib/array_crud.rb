0b959d931c1cbf0cb475a800b25318936465f2fd
Whittney Davis <wcdavis89@yahoo.com>
2019-10-30 20:50:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,8 +23,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
- array=[]
- best_pets.shift 
+ array=["wow", "I", "am", "really", "learning", "arrays!"]
+ array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
